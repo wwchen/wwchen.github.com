afdd3fece8c5c824d9a5dd02223a668551df55bf
William Chen <williamc@attentivemobile.com>
2026-02-25 14:41:20 -0600

    base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/woodworking/cabinet/',
+  base: '/wwchen.github.com/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
